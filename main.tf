5ce8b947ef0bec8c8e1a0ae6bc280e74f75cc14a
Yasin Cetindil <ycetindil@gmail.com>
2023-02-25 15:45:44 -0800

    Test

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "azurerm_virtual_machine" "vm" {
     storage_image_reference {
     publisher = "RedHat"
     offer     = "RHEL"
-    sku       = "8.4"
+    sku       = "8.6"
     version   = "latest"
   }
 
