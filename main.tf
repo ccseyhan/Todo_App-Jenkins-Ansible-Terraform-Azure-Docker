1993c9471748064bfc7f1edee21b644d7c7a86b7
Yasin Cetindil <ycetindil@gmail.com>
2023-02-25 08:37:55 -0800

    Test

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_subnet" "subnet" {
 ########################
 resource "azurerm_virtual_machine" "vm" {
   count                            = 3
-  name                             = var.vm_tags[count.index]
+  name                             = "${element(var.vm_tags, count.index )}"
   location                         = azurerm_resource_group.rg.location
   resource_group_name              = azurerm_resource_group.rg.name
   network_interface_ids            = [ azurerm_network_interface.nic[count.index].id ]
