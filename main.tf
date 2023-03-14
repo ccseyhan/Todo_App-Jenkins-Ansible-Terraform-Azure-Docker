8f5fa13ef32158e26d013718b5429e18ec13921f
Yasin Cetindil <ycetindil@gmail.com>
2023-03-14 13:32:06 -0700

    Test

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_subnet" "subnet" {
 ########################
 resource "azurerm_virtual_machine" "vm" {
   count                            = 3
-  name                             = "${element(var.vm_tags, count.index )}"
+  name                             = element(var.vm_tags, count.index)
   location                         = azurerm_resource_group.rg.location
   resource_group_name              = azurerm_resource_group.rg.name
   network_interface_ids            = [ azurerm_network_interface.nic[count.index].id ]
@@ -64,7 +64,7 @@ resource "azurerm_virtual_machine" "vm" {
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
   tags = {
-    "name" = "${element(var.vm_tags, count.index )}"
+    "name" = element(var.vm_tags, count.index)
     "environment" = "development"
   }
 
@@ -88,7 +88,7 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   os_profile {
-    computer_name  = var.prefix
+    computer_name  = element(var.vm_tags, count.index)
     admin_username = var.admin_username
   }
 
